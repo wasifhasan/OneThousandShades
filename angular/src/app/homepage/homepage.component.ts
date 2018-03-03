03df2aa59c7d6b0be1c2b5ca9e7f3b6e6b865c79
bazmi <smbazmi@gmail.com>
2018-03-03 21:19:10 +0530

    Created Home Page For Products.

--- angular/src/app/homepage/homepage.component.ts
+++ angular/src/app/homepage/homepage.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 
 @Component({
@@ -8,17 +8,34 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
-	productCatgories:any;
-	productMap:any;
-  constructor(private http:HttpClient) { }
+  productCatgories: any;
+  productMap: any;
+  title = 'Welcome To OneThousandShade';
+  images;
+  constructor(private http: HttpClient) {
+    this.images = [
+      {'url': 'assets/img/gallery/gallery-img-1-4col.jpg'},
+      {'url': 'assets/img/gallery/gallery-img-1-4col.jpg'},
+      {'url': 'assets/img/gallery/gallery-img-1-4col.jpg'},
+      {'url': 'assets/img/gallery/gallery-img-1-4col.jpg'},
+      {'url': 'assets/img/gallery/gallery-img-1-4col.jpg'},
+      {'url': 'assets/img/gallery/gallery-img-1-4col.jpg'},
+      {'url': 'assets/img/gallery/gallery-img-1-4col.jpg'},
+      {'url': 'assets/img/gallery/gallery-img-1-4col.jpg'},
+      {'url': 'assets/img/gallery/gallery-img-1-4col.jpg'},
+      {'url': 'assets/img/gallery/gallery-img-1-4col.jpg'},
+      {'url': 'assets/img/gallery/gallery-img-1-4col.jpg'},
+      {'url': 'assets/img/gallery/gallery-img-1-4col.jpg'}
+    ];
+  }
 
   ngOnInit() {
-  this.http.get('/api/productCategory').subscribe(data=>{
-	  this.productCatgories=data;
-	  });
-	 this.http.get('/api/productCategoryMap').subscribe(data=>{
-	  this.productMap=data;
-	  });
+    this.http.get('/api/productCategory').subscribe(data => {
+      this.productCatgories = data;
+    });
+    this.http.get('/api/productCategoryMap').subscribe(data => {
+      this.productMap = data;
+    });
   }
 
 }
